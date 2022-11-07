fb008dba66d982c62b884d9fb6b43da005b6c9d5
Nadezhda <nan.dudova@gmail.com>
2022-11-07 17:50:57 +0200

    commit

---
+++ _22ExamPreparationChavdarMitkov_RealDeal/src/main/java/softuni/exam/entity/Picture.java
@@ -0,0 +1,45 @@
+package softuni.exam.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pictures")
+public class Picture extends BaseEntity {
+    @Column(unique = true)
+    private String name;
+    @Column
+    private LocalDateTime localDateTime;
+    @ManyToOne
+    private Car car;
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public Picture() {
+    }
+}
