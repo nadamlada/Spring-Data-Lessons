2fd881f405f2f215a6ee05ddf20435487c19f5a3
Nadezhda <nan.dudova@gmail.com>
2022-11-14 17:20:50 +0200

    commit

--- _23ExamPreparationChvavdarMitkov_Football/src/main/java/softuni/exam/repository/PictureRepository.java
+++ _23ExamPreparationChvavdarMitkov_Football/src/main/java/softuni/exam/repository/PictureRepository.java
@@ -1,7 +1,10 @@
 package softuni.exam.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.domain.entity.Picture;
 
-public interface PictureRepository  {
-
-
+@Repository
+public interface PictureRepository  extends JpaRepository<Picture, Long> {
+    Picture findByUrl(String url);
 }
