c7bb20c2dccc807d2449074f213d622b29d2a1f0
Nadezhda <nan.dudova@gmail.com>
2022-11-16 17:29:21 +0200

    commit

---
+++ _27ExamPreparation17April2022/src/main/java/softuni/exam/models/entity/Country.java
@@ -0,0 +1,33 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "countries")
+public class Country extends BaseEntity {
+    @Column (length = 60, nullable = false, unique = true)
+    private String countryName;
+    @Column(length = 20, nullable = false)
+    private String currency;
+
+    public Country() {
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
