c397d93141ca791684ad2223e1625403c4607877
Nadezhda <nan.dudova@gmail.com>
2022-11-16 17:30:00 +0200

    commit

---
+++ _27ExamPreparation17April2022/src/main/java/softuni/exam/Paths/Paths.java
@@ -0,0 +1,7 @@
+package softuni.exam.Paths;
+
+public class Paths {
+    public static final String CITIES_FILE_PATH="src/main/resources/files/json/cities.json";
+    public static final String FORECASTS_FILE_PATH="src/main/resources/files/xml/forecasts.xml";
+    public static final String COUNTRIES_FILE_PATH ="src/main/resources/files/json/countries.json";
+}
