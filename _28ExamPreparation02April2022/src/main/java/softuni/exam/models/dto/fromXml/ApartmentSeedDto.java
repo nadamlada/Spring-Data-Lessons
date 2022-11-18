904c215d1201ac4b8994606ff665f79943c502cb
Nadezhda <nan.dudova@gmail.com>
2022-11-18 13:13:11 +0200

    to do export

---
+++ _28ExamPreparation02April2022/src/main/java/softuni/exam/models/dto/fromXml/ApartmentSeedDto.java
@@ -0,0 +1,46 @@
+package softuni.exam.models.dto.fromXml;
+
+import softuni.exam.models.entity.ApartmentType;
+
+import javax.validation.constraints.Min;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ApartmentSeedDto {
+    @XmlElement
+    private ApartmentType apartmentType;
+    @XmlElement
+    @Min(value = 40)
+    private double area;
+    @XmlElement
+    private String town;
+
+    public ApartmentSeedDto() {
+    }
+
+    public ApartmentType getApartmentType() {
+        return apartmentType;
+    }
+
+    public void setApartmentType(ApartmentType apartmentType) {
+        this.apartmentType = apartmentType;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
