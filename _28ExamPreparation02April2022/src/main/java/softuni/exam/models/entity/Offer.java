3b7a1f09a7390c922bd24de467121816b7222c84
Nadezhda <nan.dudova@gmail.com>
2022-11-19 13:14:26 +0200

    to do export

---
+++ _28ExamPreparation02April2022/src/main/java/softuni/exam/models/entity/Offer.java
@@ -0,0 +1,53 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "offers")
+public class Offer extends BaseEntity {
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(name = "published_on", nullable = false)
+    private LocalDate publishedOn;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Agent agent;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Apartment apartment;
+
+    public Offer() {
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getPublishedOn() {
+        return publishedOn;
+    }
+
+    public void setPublishedOn(LocalDate publishedOn) {
+        this.publishedOn = publishedOn;
+    }
+
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public void setAgent(Agent agent) {
+        this.agent = agent;
+    }
+
+    public Apartment getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(Apartment apartment) {
+        this.apartment = apartment;
+    }
+}
