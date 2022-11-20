1ca41eabee89402e02fadcb2b8a5443a865a029e
Nadezhda <nan.dudova@gmail.com>
2022-11-20 20:22:42 +0200

    commit

---
+++ _29ExamPreparation01August2021/src/main/java/com/example/football/models/dto/fromXml/StatSeedDto.java
@@ -0,0 +1,47 @@
+package com.example.football.models.dto.fromXml;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class StatSeedDto {
+    @XmlElement
+    @Positive
+    private Float passing;
+    @XmlElement
+    @Positive
+    private Float shooting;
+    @XmlElement
+    @Positive
+    private Float endurance;
+
+    public StatSeedDto() {
+    }
+
+    public Float getPassing() {
+        return passing;
+    }
+
+    public void setPassing(Float passing) {
+        this.passing = passing;
+    }
+
+    public Float getShooting() {
+        return shooting;
+    }
+
+    public void setShooting(Float shooting) {
+        this.shooting = shooting;
+    }
+
+    public Float getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(Float endurance) {
+        this.endurance = endurance;
+    }
+}
