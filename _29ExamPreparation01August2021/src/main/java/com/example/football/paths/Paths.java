690a6ececa47c9533d9b93c0a3be54d887a6a150
Nadezhda <nan.dudova@gmail.com>
2022-11-21 20:23:52 +0200

    commit

---
+++ _29ExamPreparation01August2021/src/main/java/com/example/football/paths/Paths.java
@@ -0,0 +1,8 @@
+package com.example.football.paths;
+
+public class Paths {
+    public static final String TEAMS_FILE_PATH = "src/main/resources/files/json/teams.json";
+    public static final String TOWNS_FILE_PATH = "src/main/resources/files/json/towns.json";
+    public static final String PLAYERS_FILE_PATH = "src/main/resources/files/xml/players.xml";
+    public static final String STATS_FILE_PATH = "src/main/resources/files/xml/stats.xml";
+}
