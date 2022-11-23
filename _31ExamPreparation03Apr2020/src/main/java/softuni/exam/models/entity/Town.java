e51f929ab9aff549040eed7ea943ee895d70effb
Nadezhda <nan.dudova@gmail.com>
2022-11-23 18:21:57 +0200

    commit

---
+++ _31ExamPreparation03Apr2020/src/main/java/softuni/exam/models/entity/Town.java
@@ -0,0 +1,43 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "towns")
+public class Town extends BaseEntity {
+    @Column
+    private String guide;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private Integer population;
+
+    public Town() {
+    }
+
+    public String getGuide() {
+        return guide;
+    }
+
+    public void setGuide(String guide) {
+        this.guide = guide;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+}
