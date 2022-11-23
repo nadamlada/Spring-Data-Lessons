ae28e8b4a8ea5091d7817831651ecaedeefe0036
Nadezhda <nan.dudova@gmail.com>
2022-11-23 18:21:34 +0200

    commit

---
+++ _31ExamPreparation03Apr2020/src/main/java/softuni/exam/models/entity/Ticket.java
@@ -0,0 +1,83 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket extends BaseEntity {
+    @Column
+    private BigDecimal price;
+    @Column(unique = true)
+    private String serialNumber;
+    @Column
+    private LocalDateTime takeOff;
+    @ManyToOne
+    private Town fromTown;
+    @ManyToOne
+    private Passenger passenger;
+    @ManyToOne
+    private Plane plane;
+    @ManyToOne
+    private Town toTown;
+
+    public Ticket() {
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public LocalDateTime getTakeOff() {
+        return takeOff;
+    }
+
+    public void setTakeOff(LocalDateTime takeOff) {
+        this.takeOff = takeOff;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+
+    public Town getFromTown() {
+        return fromTown;
+    }
+
+    public void setFromTown(Town fromTown) {
+        this.fromTown = fromTown;
+    }
+
+    public Town getToTown() {
+        return toTown;
+    }
+
+    public void setToTown(Town toTown) {
+        this.toTown = toTown;
+    }
+}
