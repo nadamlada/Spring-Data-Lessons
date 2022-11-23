22d8d30261b4d9d9eb0fb47cbcc9ae216adfbbb6
Nadezhda <nan.dudova@gmail.com>
2022-11-23 18:20:45 +0200

    commit

---
+++ _31ExamPreparation03Apr2020/src/main/java/softuni/exam/Paths/MyPaths.java
@@ -0,0 +1,8 @@
+package softuni.exam.Paths;
+
+public class MyPaths {
+    public static final String PASSENGERS_FILE_PATH = "src/main/resources/files/json/passengers.json";
+    public static final String TOWNS_FILE_PATH = "src/main/resources/files/json/towns.json";
+    public static final String PLANES_FILE_PATH = "src/main/resources/files/xml/planes.xml";
+    public static final String TICKETS_FILE_PATH = "src/main/resources/files/xml/tickets.xml";
+}
