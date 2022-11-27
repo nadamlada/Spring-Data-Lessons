4c48f371e8aa903569c6525ed1a10467c9a80d18
Nadezhda <nan.dudova@gmail.com>
2022-11-27 18:22:55 +0200

    commit

---
+++ _31ExamPreparation03Apr2020/src/main/java/softuni/exam/service/PlaneService.java
@@ -0,0 +1,18 @@
+package softuni.exam.service;
+
+import softuni.exam.models.entity.Plane;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface PlaneService {
+
+    boolean areImported();
+
+    String readPlanesFileContent() throws IOException;
+	
+	String importPlanes() throws JAXBException, FileNotFoundException;
+
+    Plane getPlaneByRegisterNumber(String registerNumber);
+}
