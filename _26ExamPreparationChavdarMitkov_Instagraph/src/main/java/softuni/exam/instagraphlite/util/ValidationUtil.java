ca8b580262a3b14328564b09e818293d99f6097d
Nadezhda <nan.dudova@gmail.com>
2022-12-07 18:18:39 +0200

    commit

--- _26ExamPreparationChavdarMitkov_Instagraph/src/main/java/softuni/exam/instagraphlite/util/ValidationUtil.java
+++ _26ExamPreparationChavdarMitkov_Instagraph/src/main/java/softuni/exam/instagraphlite/util/ValidationUtil.java
@@ -1,8 +1,5 @@
 package softuni.exam.instagraphlite.util;
 
-import javax.xml.bind.JAXBException;
-import java.io.FileNotFoundException;
-
 public interface ValidationUtil {
     <T> boolean isValid(T entity);
 }
