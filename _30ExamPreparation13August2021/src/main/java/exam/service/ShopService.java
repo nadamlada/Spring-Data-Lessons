794f597868c2d7b9a095391ddfe5a54f52def6d5
Nadezhda <nan.dudova@gmail.com>
2022-12-23 12:36:30 +0200

    commit

--- _30ExamPreparation13August2021/src/main/java/exam/service/ShopService.java
+++ _30ExamPreparation13August2021/src/main/java/exam/service/ShopService.java
@@ -1,11 +1,11 @@
 package exam.service;
 
+import exam.model.entity.Shop;
 
 import javax.xml.bind.JAXBException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-//ToDo - Implement all methods
 public interface ShopService {
 
     boolean areImported();
@@ -13,4 +13,6 @@ public interface ShopService {
     String readShopsFileContent() throws IOException;
 
     String importShops() throws JAXBException, FileNotFoundException;
+
+    Shop getShopByName(String name);
 }
