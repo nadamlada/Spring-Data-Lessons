caafcbec8bda272eaa5a41363f7fa5073e7a6604
Nadezhda <nan.dudova@gmail.com>
2022-12-23 12:36:09 +0200

    commit

---
+++ _30ExamPreparation13August2021/src/main/java/exam/paths/MyPaths.java
@@ -0,0 +1,8 @@
+package exam.paths;
+
+public class MyPaths {
+    public static final String CUSTOMERS_FILES_PATH = "src/main/resources/files/json/customers.json";
+    public static final String LAPTOPS_FILES_PATH = "src/main/resources/files/json/laptops.json";
+    public static final String SHOPS_FILES_PATH = "src/main/resources/files/xml/shops.xml";
+    public static final String TOWNS_FILES_PATH = "src/main/resources/files/xml/towns.xml";
+}
